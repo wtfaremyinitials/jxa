714d6564db39b95a65737c11eb7260a034266cbf
Brian Gonzalez <me@briangonzalez.org>
2016-06-04 10:27:56 -0700

    Apply support

--- index.js
+++ index.js
@@ -12,34 +12,49 @@ var exec = require('child_process').execSync;
 
 // Access the contents of a reference
 function dereference(path, args) {
-    try { // Run the osascript binary in inline script mode, stringifying the reference
-        var cmd = "osascript -l JavaScript -e 'JSON.stringify(" + path + "());'"
+    try {
+        // Run the osascript binary in inline script mode, stringifying the reference
+        args = args ? JSON.stringify(args) : '';
+        args = args.substr(1, args.length - 2);
+
+        var cmd = `osascript -l JavaScript -e 'JSON.stringify(${path}(${args}));'`
         var res = exec(cmd, { stdio: '' }).toString().trim();
         return JSON.parse(res);
-    } catch(e) {}
+    } catch(e) {
+      //console.log(e);
+    }
 };
 
 // Used when the node REPL calls .inspect() to print a reference
 function createInspector(path)  {
-    return () => `[object JXAReference => ${dereference(path+'.toString')}]`;
+  return () => `[object JXAReference => ${dereference(path+'.toString')}]`;
 }
 
 // Create a pointer to an object in the AppleScript API
 function createReference(path) {
     // Object being proxied is the dereference function
-    return new Proxy((recv, _, args) => dereference(path, args), {
-        // Get trap catches props being accesses, returns a new reference
-        get: (_, prop) => {
-            if(prop == 'inspect') // Handle node REPL's .inspect() calls
-                return createInspector(path);
-            return createReference(`${path}.${prop}`)
+    return new Proxy(
+      (recv, _, args) => dereference(path, args),
+      {
+        apply: (target, thisArg, argumentsList) => dereference(path, argumentsList),
+
+        get(target, propertyName, receiver) {
+           // Handle node REPL's .inspect() calls
+          if (propertyName == 'inspect') { return createInspector(path); }
+          return createReference(`${path}.${propertyName.toString()}`)
+        },
+
+        set(target, property, value, receiver) {
+          // console.log('setting: ', property);
+          // TODO: Implement `set` proxy handler
         }
-    });
+      }
+    );
 };
 
 // Entry point for module. Creates a reference to an Application()
 var Application = function(handle) {
-    return createReference('Application("' + handle + '")');
+  return createReference('Application("' + handle + '")');
 };
 
 module.exports.Application = Application;
