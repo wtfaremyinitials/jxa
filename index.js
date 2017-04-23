9e394da9d32012014b1805363c0c432047ace825
Will Franzen <wtfranzen@gmail.com>
2017-04-23 14:15:53 -0500

    Rewrite to use osa2 for interfacing with macOS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Different behavior for array specifiers over object specifiers?
 
-// The AppleScript API is accessed through the `osascript` binary
-var exec = require('child_process').execSync;
+var osa = require('osa2')
+var wait = require('blocking-await')
 
 function isInspect(obj) {
     var symb = require('util').inspect.custom
@@ -10,59 +10,51 @@ function isInspect(obj) {
 
 function parent(path) {
     // TODO: May need to handle func calls and . accessor
-    return path.replace(/\["\w+"\]$/, '');
+    return path.replace(/\["\w+"\]$/, '')
 }
 
 // Access the contents of a reference
 function dereference(path, args) {
-    try { // Run the osascript binary in inline script mode, stringifying the reference
-        var cmd;
- 
-        // Arguments passed?
-        if(args.length == 0) {
-            cmd = `${path}()`; // Don't bother with apply
+    return wait((osa(function(path, that, args) {
+        var target = eval(path)
+
+        if (args.length != 0 || path.endsWith('toString')) {
+            obj = target.apply(eval(that), args)
         } else {
-            // Stringify the arguments then call the method with `apply`
-            args = JSON.stringify(args);
-            cmd = `${path}.apply(${parent(path)}, JSON.parse(\`${args}\`))`;
+            obj = target()
         }
 
-        // Add in code to test if the resulting output should be stringified (object specifiers should not)
-        cmd = `obj=${cmd};if(!(/\\[object \\w+Specifier\\]/.test(obj.toString())))obj=JSON.stringify(obj);obj`
-        // Wrap it as a command
-        cmd = `osascript -l JavaScript -e '${cmd}'`;
-        // Run it
-        var res = exec(cmd, { stdio: '' }).toString().trim();
+        if (obj == null)
+            return null
 
-        // Create a reference if an object specifier was returned
-        if(res.startsWith('Application'))
-            return createReference(res);
-        // Otherwise parse the result
-        return JSON.parse(res);
-    } catch(e) {}
-};
+        if (/\\[object \\w+Specifier\\]/.test(obj.toString()))
+            obj = obj.toString()
+
+        return obj
+    }))(path, parent(path), args))
+}
 
 // Used when the node REPL calls .inspect() to print a reference
 function createInspector(path)  {
-    return () => `[object JXAReference => ${dereference(path+'.toString',[])}]`;
+    return () => `[object JXAReference => ${dereference(path+'.toString',[])}]`
 }
 
 // Create a pointer to an object in the AppleScript API
 function createReference(path) {
     // Object being proxied is the dereference function
     return new Proxy((...args) => dereference(path, args), {
-        // Get trap catches props being accesses, returns a new reference
+        // Get trap catches props being accessed, returns a new reference
         get: (_, prop) => {
             if(isInspect(prop)) // Handle node REPL's .inspect() calls
-                return createInspector(path);
+                return createInspector(path)
             return createReference(`${path}["${prop}"]`)
         }
     });
 };
 
 // Entry point for module. Creates a reference to an Application()
-var Application = function(handle) {
-    return createReference('Application("' + handle + '")');
-};
+function Application(handle) {
+    return createReference(`Application("${handle}")`)
+}
 
-module.exports.Application = Application;
+module.exports.Application = Application
